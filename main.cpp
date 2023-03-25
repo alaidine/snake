d27183b5c9e382d1c24ae04db3718712fe6dc648
alaidine <aa.alimlinde@gmail.com>
2023-03-25 22:21:51 +0400

    the program now render a triangle that moves in a direction and this direciton changes when a button is pushed

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main() {
 		return -1;
 	}
 
-	SDL_Window *window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 680, 480, 0);
+	SDL_Window *window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 500, 0);
 	if (!window) {
 		std::cout << "Failed to create window" << std::endl;
 		return -1;
@@ -16,30 +16,73 @@ int main() {
 	// renderer is to draw on the window
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 	if (!renderer) {
-		std::cout << "Failed to create renderer" << std::end;
+		std::cout << "Failed to create renderer" << std::endl;
 		return -1;
 	}
 
-	// window_surface is to render an image into the screen
-	SDL_Surface *window_surface = SDL_GetWindowSurface(window);
-	if(!window_surface) {
-		std::cout << "Failed to get the surface from the window" << std::endl;
-		return -1;
-	}
-
-	// main loop
+	enum Direction {
+		DOWN,
+		UP,
+		LEFT,
+		RIGHT
+	};
+	int dir = 0;
+	SDL_Rect head {250, 250, 10, 10};
 	bool keep_window_open = true;
 	while(keep_window_open) {
 		SDL_Event e;
+
+		// Check input
 		while(SDL_PollEvent(&e)) {
 			switch(e.type) {
 				case SDL_QUIT:
 					keep_window_open = false;
 					break;
+				case SDL_KEYDOWN:
+					switch(e.key.keysym.sym) {
+						case SDLK_DOWN:
+							dir = DOWN;
+							break;
+						case SDLK_UP:
+							dir = UP;
+							break;
+						case SDLK_LEFT:
+							dir = LEFT;
+							break;
+						case SDLK_RIGHT:
+							dir = RIGHT;
+							break;
+				}
+					
 			}
 		}
-		SDL_UpdateWindowSurface(window);
-	}
 
-	return 0;
+		// Move
+		switch(dir) {
+			case DOWN:
+				head.y += 5;
+				break;
+			case UP:
+				head.y -= 5;
+				break;
+			case LEFT:
+				head.x -= 5;
+				break;
+			case RIGHT:
+				head.x += 5;
+				break;
+		}
+
+		// Clear Window with a color
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderClear(renderer);
+
+		// Draw the body
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		SDL_RenderFillRect(renderer, &head);
+
+		// Present the renderer to show what has been drawn
+		SDL_RenderPresent(renderer);
+		SDL_Delay(25);
+	}
 }
